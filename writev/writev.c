94075cebc774efe5712b1a4808a978f3154459a7
Aymeric De Cocq <aymeric.decocq@student.uclouvain.be>
2014-05-15 17:37:03 +0200

    correction 6 writev.c

--- writev/writev.c
+++ writev/writev.c
@@ -42,7 +42,7 @@ void writeVBenchmark (int length, char* buf0, timer* t, recorder* r) {
     fsync(fd);
     
     //Fin du timer
-    write_record(r,nFiles,stop_timer(t));
+    write_record(r,length,stop_timer(t));
     
     close(fd);
 }
@@ -64,7 +64,7 @@ void writeLseekBenchmark (int length, char* buf0, timer* t, recorder* r) {
     fsync(fd);
     
     //Fin du timer
-    write_record(r,nFiles,stop_timer(t));
+    write_record(r,length,stop_timer(t));
     
     close(fd);
     
@@ -82,7 +82,7 @@ int main()
     char *buf0 = "gqtLsUKFyp1GZdwpZN9CFQncmWAvsC1TGbJ68Hz3RbFrvHIZjkfXRvKObFKyvfzQ0YHCWTDteVsvjJtCxa6Zlsrb8Du3LJYlqtJygwuyPddL557q91YiPmN9svHgrx0kjiiSqrKoVvKgZlAIEDBrvg0TlIaSwU0jjRQnRK1mzqQxS92y93Rv06Oj0j1Uw8GP9TuLwj9KroxnGRFGSZRwtlHkz3avykLTvnpg1suxRdQb0x6rJNsUb5GgvkMxvqDpvdjlfoCc8YjPT5q9YXtzN98WfYIVAqZc29pqFDo7ZKgvuudzPPKJK2tv2kAo4uEmNotlTqRLBuewju8LIX4tMJmSi4a8RpSkZWB8GGQmGP7ItWWpzUSiU4hpYxqO4KPdBDhcnti8FAK1Ri0nTegYnT6dHcNQi7LDyRTPlpldyx0umntZsR2hZAVrcdK9taeg8yrtFgDO51DUuOFAD9k1RejpqTV4vGseswj2kmGPfGk8wu29Ivos61ASwecHjD8M6bDovQqpkFtbfnts1r8bCNtb4csZV5UmlqM3COONk98wX1iUPN09RbsSQjXiiN6srxgyOWKhj2F2CIloGAkbrOFnQ9BDpfmSZJkJg6NQrToWcfrOO7sWgIYzoFwvHwaqZug9Ya3ntRl1SOD31faqCR7eqKjQiTjX8g8xdB1gXq6a8iHzgaf6J7uMXR6gjbXRYa93NrE78flMjbofkQJvMjCbC4h02eyMEmvJfPIjiIFXq0wLR801ORo7HGB0G5bIloYCVej3uds3y5oOjR9C5O4YxfxVx6YyPJCqpmAY2imNYexyhyAdKadXcjlod69K5rWiDGcW6UZiBTrqJZQcckN0SHZxjWDzPYzI6mb5ir4JXcqcRzctowZ6xTMOtJmwI8RfrA2APgdhqqSwBxteqM8f94wsrJwHLuxHxr0jZHKNOLaAGlpReRaL06kRchI10O1ITxRRDLj5zy7wWUBczXpUlNgkEGFLDsUgg4ma9bT5sXZL4SslECrgNYDpMqn\n";
 
     writeVBenchmark(1000, buf0,t,writev_rec);
-    writeLseekBenchmark(1000, buf0),t,writelseek_rec);
+    writeLseekBenchmark(1000, buf0,t,writelseek_rec);
     
     recorder_free(writev_rec);
     recorder_free(writelseek_rec);
