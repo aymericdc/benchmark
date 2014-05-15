394a3e5ffbaba0bff0f77cb6702d107ec9c3e4ab
Léonard Julémont <leonard.julemont@student.uclouvain.be>
2014-05-15 18:05:11 +0200

    modifications fonctionnent sur salle info

--- readdir/readdir.c
+++ readdir/readdir.c
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <dirent.h>
+#include <unistd.h>
+
+#include "benchmark.h"
 
 
 void benchmarkReaddir(char * dirName, int nFiles,timer* t, recorder* r)
@@ -24,10 +27,11 @@ void benchmarkReaddir(char * dirName, int nFiles,timer* t, recorder* r)
         
         int fd = creat(str2,S_IWUSR|S_IRUSR);
         if(fd==-1){
-            perror("open");
+            perror("creat in benchmarkReaddir");
             exit(EXIT_FAILURE);
         }
     }
+    
     
     //Ouverture du dossier
     DIR *dirp;
@@ -35,11 +39,10 @@ void benchmarkReaddir(char * dirName, int nFiles,timer* t, recorder* r)
     
     dirp = opendir(dirName);
     if(dirp==NULL) {
-        perror("opendir");
+        perror("opendir in benchmarkReaddir");
         exit(EXIT_FAILURE);
     }
     
-	
 	//lance le timer
 	start_timer(t);
     while ((dp = readdir(dirp)) != NULL) {
@@ -48,10 +51,11 @@ void benchmarkReaddir(char * dirName, int nFiles,timer* t, recorder* r)
 	//on ecrit le temps moyen pour nfile fichiers, par fichier
     write_record_n(r,nFiles,stop_timer(t),nFiles);
 	
+	
 	//fermeture du dossier
     err = closedir(dirp);
     if(err<0) {
-        perror("closedir");
+        perror("closedir in benchmarkReaddir");
         exit(EXIT_FAILURE);
     }
     
@@ -67,11 +71,10 @@ void benchmarkReaddir(char * dirName, int nFiles,timer* t, recorder* r)
         
         err = unlink(str2);
         if(err==-1){
-            perror("unlink");
+            perror("unlink in benchmarkReaddir");
             exit(EXIT_FAILURE);
         }
     }	
-	
 }
 
 int main(int argc, const char * argv[])
@@ -79,26 +82,33 @@ int main(int argc, const char * argv[])
     timer *t = timer_alloc();
 	recorder *readdir_rec = recorder_alloc("readdir.csv");
 	
+	//creation du dossier pour les fichiers
     char dirName[]="temp";
     
     int err = mkdir(dirName, S_IRUSR|S_IWUSR|S_IXUSR);
     if(err==-1){
-        perror("mkdir");
+        perror("mkdir in main");
         exit(EXIT_FAILURE);
     }
     
-	
+	//lancement des tests
 	benchmarkReaddir(dirName,1,t,readdir_rec);
 	benchmarkReaddir(dirName,10,t,readdir_rec);
 	benchmarkReaddir(dirName,50,t,readdir_rec);
 	benchmarkReaddir(dirName,100,t,readdir_rec);
+	benchmarkReaddir(dirName,200,t,readdir_rec);
+	benchmarkReaddir(dirName,500,t,readdir_rec);
+	benchmarkReaddir(dirName,1000,t,readdir_rec);
 	
     
+    /* Ne pas fonctionne pas a cause de fichiers .nfs
     err = rmdir(dirName);
     if(err==-1){
-        perror("rmdir");
+        perror("rmdir in main");
         exit(EXIT_FAILURE);
     }
+    */
+	
     
 	recorder_free(readdir_rec);
 	timer_free(t);
