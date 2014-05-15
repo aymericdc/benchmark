2657b1815305ecc21c482d392772a72b4883267f
Aymeric De Cocq <aymeric.decocq@student.uclouvain.be>
2014-05-15 16:57:49 +0200

    Correction 2 de write V plus ajout des timer

--- writev/writev.c
+++ writev/writev.c
@@ -14,9 +14,9 @@
 #include <sys/uio.h>
 
 /*
- Fonction
+ Fonction pour le test de write v. Elle prend en argument le nombre de fois que on ecrit le string passe en deuxiemme argument
  */
-void writetest (int length, char* buf0) {
+void writeVBenchmark (int length, char* buf0, timer* t, recorder* r) {
     ssize_t bytes_written;
     int fd = open("writeV.txt",  O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH);
     
@@ -31,26 +31,40 @@ void writetest (int length, char* buf0) {
     iovcnt = length;
     
     int j;
-    //start timer
+    
+    //Debut du timer
+	start_timer(t);
+    
+    //Ecriture
     for (j=0; j<length; j++) {};
     bytes_written = writev(fd, iov, iovcnt);
     fsync(fd);
-    //stop timer
+    
+    //Fin du timer
+    write_record(r,nFiles,stop_timer(t));
+    
     close(fd);
 }
 
-void writelseek (int length, char* buf0) {
+
+/*
+ Fonction pour le test de write. Elle prend en argument le nombre de fois que on ecrit le string passe en deuxiemme argument
+ */
+void writeLseekBenchmark (int length, char* buf0) {
     int fd = open("writeLseek.txt",  O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH);
-    
-    
     int i;
-    //start timer
+    //Debut du timer
+	start_timer(t);
+    
     for (i=0; i<length; i++) {
         write(fd, buf0, strlen(buf0));
         //lseek(fd, (off_t) strlen(buf0), SEEK_CUR);
     }
     fsync(fd);
-    //stop timer
+    
+    //Fin du timer
+    write_record(r,nFiles,stop_timer(t));
+    
     close(fd);
     
     
@@ -58,7 +72,18 @@ void writelseek (int length, char* buf0) {
 
 int main()
 {
+    
+    timer *t = timer_alloc();
+    recorder *writev_rec = recorder_alloc("writev.csv");
+    recorder *writelseek_rec = recorder_alloc("writev.csv");
+    
+    //String de taille 1024
     char *buf0 = "gqtLsUKFyp1GZdwpZN9CFQncmWAvsC1TGbJ68Hz3RbFrvHIZjkfXRvKObFKyvfzQ0YHCWTDteVsvjJtCxa6Zlsrb8Du3LJYlqtJygwuyPddL557q91YiPmN9svHgrx0kjiiSqrKoVvKgZlAIEDBrvg0TlIaSwU0jjRQnRK1mzqQxS92y93Rv06Oj0j1Uw8GP9TuLwj9KroxnGRFGSZRwtlHkz3avykLTvnpg1suxRdQb0x6rJNsUb5GgvkMxvqDpvdjlfoCc8YjPT5q9YXtzN98WfYIVAqZc29pqFDo7ZKgvuudzPPKJK2tv2kAo4uEmNotlTqRLBuewju8LIX4tMJmSi4a8RpSkZWB8GGQmGP7ItWWpzUSiU4hpYxqO4KPdBDhcnti8FAK1Ri0nTegYnT6dHcNQi7LDyRTPlpldyx0umntZsR2hZAVrcdK9taeg8yrtFgDO51DUuOFAD9k1RejpqTV4vGseswj2kmGPfGk8wu29Ivos61ASwecHjD8M6bDovQqpkFtbfnts1r8bCNtb4csZV5UmlqM3COONk98wX1iUPN09RbsSQjXiiN6srxgyOWKhj2F2CIloGAkbrOFnQ9BDpfmSZJkJg6NQrToWcfrOO7sWgIYzoFwvHwaqZug9Ya3ntRl1SOD31faqCR7eqKjQiTjX8g8xdB1gXq6a8iHzgaf6J7uMXR6gjbXRYa93NrE78flMjbofkQJvMjCbC4h02eyMEmvJfPIjiIFXq0wLR801ORo7HGB0G5bIloYCVej3uds3y5oOjR9C5O4YxfxVx6YyPJCqpmAY2imNYexyhyAdKadXcjlod69K5rWiDGcW6UZiBTrqJZQcckN0SHZxjWDzPYzI6mb5ir4JXcqcRzctowZ6xTMOtJmwI8RfrA2APgdhqqSwBxteqM8f94wsrJwHLuxHxr0jZHKNOLaAGlpReRaL06kRchI10O1ITxRRDLj5zy7wWUBczXpUlNgkEGFLDsUgg4ma9bT5sXZL4SslECrgNYDpMqn\n";
-    writetest(1000, buf0);
-    writelseek(1000, buf0);
+
+    writeVBenchmark(1000, buf0,t,writev_rec);
+    writeLseekBenchmark(1000, buf0),t,writelseek_rec);
+    
+    recorder_free(writev_rec);
+    recorder_free(writelseek_rec);
+	timer_free(t);
 }
